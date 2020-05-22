a2fccaff71b503c59ef405257b4b049f4b48e7bb
Rohith Reddy <5032439+brandedoutcast@users.noreply.github.com>
2020-05-22 07:40:14 +0530

    fix #31 remove nupkg deletion before dotnet pack

--- index.js
+++ index.js
@@ -55,8 +55,6 @@ class Action {
 
         console.log(`NuGet Source: ${this.nugetSource}`)
 
-        fs.readdirSync(".").filter(fn => fn.endsWith("nupkg")).forEach(fn => fs.unlinkSync(fn))
-
         this._executeInProcess(`dotnet build -c Release ${this.projectFile}`)
 
         this._executeInProcess(`dotnet pack ${this.includeSymbols ? "--include-symbols -p:SymbolPackageFormat=snupkg" : ""} --no-build -c Release ${this.projectFile} -o .`)
