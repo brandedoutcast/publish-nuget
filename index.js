4637c3bdd3fb4c052235299664c57b14c398cbd0
Rohith Reddy <5032439+brandedoutcast@users.noreply.github.com>
2020-07-10 14:41:58 +0530

    fix #36 update to stricter version regexp to ignore comments

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ class Action {
         this.projectFile = process.env.INPUT_PROJECT_FILE_PATH
         this.packageName = process.env.INPUT_PACKAGE_NAME || process.env.PACKAGE_NAME
         this.versionFile = process.env.INPUT_VERSION_FILE_PATH || process.env.VERSION_FILE_PATH || this.projectFile
-        this.versionRegex = new RegExp(process.env.INPUT_VERSION_REGEX || process.env.VERSION_REGEX)
+        this.versionRegex = new RegExp(process.env.INPUT_VERSION_REGEX || process.env.VERSION_REGEX, "m")
         this.version = process.env.INPUT_VERSION_STATIC || process.env.VERSION_STATIC
         this.tagCommit = JSON.parse(process.env.INPUT_TAG_COMMIT || process.env.TAG_COMMIT)
         this.tagFormat = process.env.INPUT_TAG_FORMAT || process.env.TAG_FORMAT
